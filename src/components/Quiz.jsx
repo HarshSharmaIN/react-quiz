d94ffb6521dd13f53cb50a9a5fdbfa2644c2e6bd
HarshSharmaIN <harshsharmaasl80@gmail.com>
2024-04-06 11:03:57 +0530

    fix bug

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,31 +1,31 @@
-import { useState, useCallback } from "react";
-import QUESTIONS from "../questions.js";
-import Question from "./Question.jsx";
-import Summary from "./Summary.jsx";
+import { useState, useCallback } from 'react';
+
+import QUESTIONS from '../questions.js';
+import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 export default function Quiz() {
-  const [userAnswer, setUserAnswer] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswer.length;
-  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
-    setUserAnswer((prevUserAnswer) => {
-      return [...prevUserAnswer, selectedAnswer];
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
     });
   },
   []);
 
-  const handleSkipAnswer = useCallback(() => {
-    handleSelectAnswer(null);
-  }, [handleSelectAnswer]);
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
-  if (quizIsCompleted) {
-    return (
-      <Summary userAnswers={userAnswer} />
-    );
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />
   }
 
   return (
